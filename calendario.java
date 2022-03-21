5887473717808b40d622f9d274c731980a9fdb21
Vinicio Oliveira <oliveiravinicios31@gmail.com>
2022-03-21 14:24:33 -0300

    Fiz um calendário para data e hora

---
+++ calendario.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class calendario {
+    public static void main(String[] args) {
+        Date relogio = new Date();
+        System.out.println("Olá, confira a data e hora abaixo:");
+        System.out.println(relogio.toString());
+    }
+}
