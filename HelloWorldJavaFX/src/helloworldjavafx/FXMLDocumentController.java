67964b2f4f2ba86071a100c74bf33739d07fb800
Vinicio Oliveira <oliveiravinicios31@gmail.com>
2022-03-24 14:33:28 -0300

    App Hello World feito em JavaFX

---
+++ HelloWorldJavaFX/src/helloworldjavafx/FXMLDocumentController.java
@@ -0,0 +1,31 @@
+package helloworldjavafx;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author vinic
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    @FXML
+    private Label lblMensagem;
+    private Button btnClique;
+    
+    @FXML
+    private void clicouBotao(ActionEvent event) {
+        lblMensagem.setText("Olá, Mundo!!");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
